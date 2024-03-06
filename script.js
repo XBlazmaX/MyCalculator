40e4aebad59dc70081fdd5ca659e7c100581c6b2
Dev Abdulaziz <126620929+XBlazmaX@users.noreply.github.com>
2024-03-07 00:49:22 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var secret = "JICCTF{G1tHub_C0mm1t5_4r3_Imp0rt4nt}";
+// Determines button clicked via id
+function myFunction(id) {
+    document.calc.result.value+=id;
+  }
+  
+  // Clears calculator input screen
+  function clearScreen() {
+    document.calc.result.value="";
+  }
+  
+  // Calculates input values
+  function calculate() {
+    try {
+      var input = eval(document.calc.result.value);
+      document.calc.result.value=input;
+    } catch(err){
+        document.calc.result.value="Error";
+      }
+  }
+
+
